04a1381dc7f6dfa353980e3e33e965d527e4e8e1
Max Richter <jim-x@web.de>
2019-08-23 00:38:31 +0200

    Created types

---
+++ src/types.ts
@@ -0,0 +1,62 @@
+interface stageConfig {
+  title: string;
+  children: UIConfig[];
+}
+
+interface Vector {
+  x: number;
+  y: number;
+}
+
+interface curveOutput {
+  points: Vector[];
+}
+
+interface parameter {
+  value: number;
+  min?: number;
+  max?: number;
+  curve?: curveOutput;
+}
+
+interface stemDescription {
+  diameter: parameter;
+  height: parameter;
+}
+
+interface branchDescription {
+  diameter: parameter;
+}
+
+interface leafDescription {
+  diameter: parameter;
+}
+
+interface plantDescription {
+  stem: stemDescription;
+  branches: branchDescription;
+  leaves: leafDescription;
+}
+
+interface Stage {
+  plantDescription: plantDescription;
+  title: string;
+  wrapper: HTMLDivElement;
+  config: stageConfig;
+  show(): void;
+  hide(): void;
+  onActivate(cb: Function): void;
+}
+
+interface UIConfig {
+  title: string;
+  type: string;
+  onUpdate?: Function;
+  children?: UIConfig[];
+}
+
+interface Button {
+  wrapper: HTMLElement;
+  active: boolean;
+  onClick(cb: Function): void;
+}
