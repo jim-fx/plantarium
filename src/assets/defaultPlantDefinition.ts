4ca7ad4c72d4369fac24a7f98558ade2fdb04dbc
Max Richter <jim-x@web.de>
2019-08-26 01:47:53 +0200

    Add default Plant Description

---
+++ src/assets/defaultPlantDefinition.ts
@@ -0,0 +1,22 @@
+const pd: plantDescription = {
+  stem: {
+    diameter: {
+      value: 10
+    },
+    height: {
+      value: 10
+    }
+  },
+  branches: {
+    diameter: {
+      value: 2
+    }
+  },
+  leaves: {
+    diameter: {
+      value: 1
+    }
+  }
+};
+
+export default pd;
