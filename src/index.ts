0aa6804b2b2128ac5047d16280450d880ad83846
Max Richter <jim-x@web.de>
2019-08-26 01:47:20 +0200

    Connect all the layers

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,29 @@ import Stage from "./components/stages/stageClass";
 import StageHandler from "./components/stages/stageHandler";
 import stagesConfig from "./components/stages/config";
 
-import "./components/display";
+import display from "./components/display";
+import { importer, exporter } from "./components/io";
 
-const pd = {
-  stem: {}
-};
+import defaultPD from "./assets/defaultPlantDefinition";
 
-const stages = stagesConfig
-  .map(stageConfig => new Stage(stageConfig, pd))
-  .forEach(_stage => StageHandler.registerStage(_stage));
+const stages: Stage[] = stagesConfig.map(stageConfig => new Stage(stageConfig));
+
+stages.forEach((_stage: Stage) => StageHandler.registerStage(_stage));
+
+stages.unshift(importer);
+
+stages.push(display);
+
+stages.push(exporter);
+
+stages.forEach((stage: Stage, i: number) => {
+  if (i < stages.length - 1) {
+    stage.connect(stages[i + 1]);
+  }
+});
+
+console.log(stages);
+
+importer.pd = defaultPD;
 
 resizeTables(<HTMLTableElement>document.querySelector("table"));
