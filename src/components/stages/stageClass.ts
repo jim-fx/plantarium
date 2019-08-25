30b6a3f4c2464392ed83db6f31d113be7d1905fc
Max Richter <jim-x@web.de>
2019-08-26 01:49:32 +0200

    Again more data flow

--- src/components/stages/stageClass.ts
+++ src/components/stages/stageClass.ts
@@ -1,7 +1,5 @@
 import "./stage.scss";
-import * as elements from "../../elements";
 import createFromConfig from "../../elements/createFromConfig";
-const topbar = <HTMLElement>document.getElementById("topbar");
 const sidebar = <HTMLElement>document.getElementById("sidebar");
 
 export default class Stage implements Stage {
@@ -9,21 +7,15 @@ export default class Stage implements Stage {
   config: stageConfig;
   wrapper: HTMLDivElement;
 
-  private _plantDescription: plantDescription;
   private _listeners: Function[] = [];
-  private button: elements.Button;
+  private _nextStage: Stage | undefined;
+  private _pd: plantDescription | undefined;
 
-  constructor(config: stageConfig, pd: plantDescription) {
+  constructor(config: stageConfig) {
     this.title = config.title;
     this.config = config;
     this.wrapper = document.createElement("div");
     this.wrapper.classList.add("stage-wrapper");
-    this._plantDescription = pd;
-
-    this.button = new elements.Button(topbar, this.title);
-    this.button.onClick(() => {
-      this.show();
-    });
 
     //Create all the elements (sliders, curves...)
     createFromConfig(this);
@@ -32,25 +24,29 @@ export default class Stage implements Stage {
   }
 
   show() {
-    this._listeners.forEach(cb => cb());
-    this.button.active = true;
     this.wrapper.classList.add("stage-wrapper-visible");
   }
 
   hide() {
-    this.button.active = false;
     this.wrapper.classList.remove("stage-wrapper-visible");
+  }
+
+  connect(stage: Stage) {
+    this._nextStage = stage;
   }
 
   onActivate(cb: Function) {
     this._listeners.push(cb);
   }
 
-  get plantDescription() {
-    return this._plantDescription;
+  get pd() {
+    return this._pd;
   }
 
-  set plantDescription(v) {
-    this._plantDescription = v;
+  set pd(v) {
+    this._pd = v;
+    if (this._nextStage) {
+      this._nextStage.pd = this._pd;
+    }
   }
 }
--- src/components/stages/stageHandler.ts
+++ src/components/stages/stageHandler.ts
@@ -2,24 +2,57 @@ import logger from "../../logger";
 
 const log = logger("stage handler");
 
-let activeStage: Stage;
 const stages = [];
+const topbar = <HTMLElement>document.getElementById("topbar");
+
+let activeStage: Stage;
+let activeButton: HTMLButtonElement;
+
+let activeButtonBackground = document.createElement("div");
+activeButtonBackground.classList.add("button-background");
+topbar.append(activeButtonBackground);
 
 export default {
   registerStage: (stage: Stage) => {
+    const button = document.createElement("button");
+    button.innerHTML = stage.config.title;
+    button.addEventListener(
+      "click",
+      () => {
+        if (activeStage !== stage) {
+          log("activating stage " + stage.title, 2);
+
+          activeStage.hide();
+          activeStage = stage;
+          activeStage.show();
+
+          activeButton.classList.remove("button-active");
+          activeButton = button;
+          activeButton.classList.add("button-active");
+
+          const bounds = activeButton.getBoundingClientRect();
+          activeButtonBackground.style.width = bounds.width + "px";
+          activeButtonBackground.style.left = bounds.left + "px";
+        }
+      },
+      false
+    );
+
     if (!activeStage) {
       activeStage = stage;
+      activeButton = button;
+      activeButton.classList.add("button-active");
+
       stage.show();
+
+      setTimeout(() => {
+        const bounds = activeButton.getBoundingClientRect();
+        activeButtonBackground.style.width = bounds.width + "px";
+        activeButtonBackground.style.left = bounds.left + "px";
+      }, 1);
     }
 
-    stage.onActivate(() => {
-      log("activating stage " + stage.title, 2);
-      if (activeStage !== stage) {
-        activeStage.hide();
-        activeStage = stage;
-      }
-    });
-
+    topbar.append(button);
     stages.push(stage);
   }
 };
