637334d060ced00b9e3b18e022ed6114d9088a9a
Max Richter <jim-x@web.de>
2019-08-26 01:49:13 +0200

    Make the button a standard UIElement

--- src/elements/button.ts
+++ src/elements/button.ts
@@ -1,17 +1,17 @@
 import "./button.scss";
 
-export default class Button implements Button {
-  wrapper: HTMLElement;
+import UIElement from "./element";
 
+export default class UIButton extends UIElement implements Button {
   private listeners: Function[];
   private element: HTMLButtonElement;
-  private _active: Boolean = false;
 
-  constructor(wrapper: HTMLElement, text: string = "Button") {
-    this.wrapper = wrapper;
+  constructor(stage: Stage, wrapper: HTMLElement, config: UIConfig) {
+    super(stage, wrapper, config);
 
     this.element = document.createElement("button");
-    this.element.innerHTML = text;
+    this.element.classList.add("button");
+    this.element.innerHTML = config.title;
     this.element.addEventListener(
       "click",
       () => {
@@ -23,18 +23,6 @@ export default class Button implements Button {
     this.wrapper.append(this.element);
 
     this.listeners = [];
-  }
-
-  get active() {
-    return this._active;
-  }
-
-  set active(v) {
-    v
-      ? this.element.classList.add("button-active")
-      : this.element.classList.remove("button-active");
-
-    this._active = v;
   }
 
   onClick(cb: Function) {
