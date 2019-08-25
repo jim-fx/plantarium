135d1e3a3ebbb6c456762a37c42d9a50f5c459d9
Max Richter <jim-x@web.de>
2019-08-26 01:52:14 +0200

    Add some stuff

---
+++ src/helpers/throttle.ts
@@ -0,0 +1,20 @@
+export default (func: Function, limit: number) => {
+  let lastFunc: NodeJS.Timeout;
+  let lastRan: number;
+  return function() {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function() {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
