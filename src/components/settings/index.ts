a5d7470b6211dcdb31baa5fb1853effb30a517ee
Max Richter <jim-x@web.de>
2019-08-28 17:20:00 +0200

    Added settings component

---
+++ src/components/settings/index.ts
@@ -0,0 +1,41 @@
+import importer from "../io/importer";
+import exporter from "../io/exporter";
+const settings: Map<string, any> = new Map();
+
+let obj: any = {};
+
+const save = () => {
+  obj = {};
+  settings.forEach((v, k) => {
+    obj[k] = v;
+  });
+  localStorage.setItem("pdSettings", JSON.stringify(obj));
+};
+
+const load = () => {
+  if ("pdSettings" in localStorage) {
+    obj = JSON.parse(localStorage["pdSettings"]);
+    Object.keys(obj).forEach(k => {
+      settings.set(k, obj[k]);
+    });
+  } else {
+    localStorage.setItem("pdSettings", "{}");
+  }
+};
+
+load();
+
+export default {
+  get: (key: string) => {
+    return settings.get(key);
+  },
+  set: (key: string, value: any) => {
+    settings.set(key, value);
+    save();
+    importer.init(exporter.pd);
+  },
+
+  get object(): settings {
+    return obj;
+  }
+};
