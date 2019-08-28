2940c5dd1694f03214db8a51559a844b6a9a685c
Max Richter <jim-x@web.de>
2019-08-28 17:12:55 +0200

    Add json import to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import scss from "rollup-plugin-scss";
+import json from "rollup-plugin-json";
 import { uglify } from "rollup-plugin-uglify";
 import liveServer from "rollup-plugin-live-server";
 
@@ -9,48 +10,89 @@ const PROD = process.env.NODE_ENV === "production";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
-export default {
-  input: "./src/index.ts",
+export default [
+  {
+    input: "./src/index.ts",
 
-  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
-  // https://rollupjs.org/guide/en#external-e-external
-  external: [],
+    // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+    // https://rollupjs.org/guide/en#external-e-external
+    external: [],
 
-  plugins: [
-    babel({
-      extensions: extensions
-    }),
+    plugins: [
+      babel({
+        extensions: extensions
+      }),
 
-    // will output compiled styles to bundle.css
-    scss(),
+      json(),
 
-    // Allows node_modules resolution
-    resolve({ extensions }),
+      // will output compiled styles to bundle.css
+      scss(),
 
-    // Allow bundling cjs modules. Rollup doesn't understand cjs
-    commonjs({
-      sourceMap: !PROD
-    }),
+      // Allows node_modules resolution
+      resolve({ extensions }),
 
-    PROD && uglify(),
+      // Allow bundling cjs modules. Rollup doesn't understand cjs
+      commonjs({
+        sourceMap: !PROD
+      }),
 
-    !PROD &&
-      liveServer({
-        root: "./build",
-        open: true,
-        logLevel: 0
-      })
-  ],
+      PROD && uglify(),
 
-  output: [
-    {
-      file: "./build/bundle.js",
-      format: "iife",
-      name: "plantGenerator",
-      sourcemap: PROD ? false : "inline",
+      !PROD &&
+        liveServer({
+          root: "./build",
+          open: true,
+          logLevel: 0
+        })
+    ],
 
-      // https://rollupjs.org/guide/en#output-globals-g-globals
-      globals: {}
-    }
-  ]
-};
+    output: [
+      {
+        file: "./build/bundle.js",
+        format: "iife",
+        name: "plantGenerator",
+        sourcemap: PROD ? false : "inline",
+
+        // https://rollupjs.org/guide/en#output-globals-g-globals
+        globals: {}
+      }
+    ]
+  },
+  {
+    input: "./src/components/model-generator",
+
+    // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+    // https://rollupjs.org/guide/en#external-e-external
+    external: [],
+
+    plugins: [
+      babel({
+        extensions: extensions
+      }),
+
+      json(),
+
+      // Allows node_modules resolution
+      resolve({ extensions }),
+
+      // Allow bundling cjs modules. Rollup doesn't understand cjs
+      commonjs({
+        sourceMap: !PROD
+      }),
+
+      PROD && uglify()
+    ],
+
+    output: [
+      {
+        file: "./build/generator.js",
+        format: "iife",
+        name: "plantGenerator",
+        sourcemap: PROD ? false : "inline",
+
+        // https://rollupjs.org/guide/en#output-globals-g-globals
+        globals: {}
+      }
+    ]
+  }
+];
