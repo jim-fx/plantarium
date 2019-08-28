4b33d9bc5522f11eb0bd1e1b7795cafb896484b4
Max Richter <jim-x@web.de>
2019-08-28 17:20:33 +0200

    Save open/close state of groups in localStorage

--- src/elements/group.ts
+++ src/elements/group.ts
@@ -1,41 +1,76 @@
 import "./group.scss";
 
+const groupState = "groupState" in localStorage ? JSON.parse(localStorage.groupState) : {};
+let incrementer = 0;
+
 export default class Group {
-  wrapper: HTMLElement;
+  wrapper: HTMLElement = document.createElement("div");
   private open: boolean = false;
-  constructor(wrapper: HTMLElement, title: string) {
-    this.wrapper = document.createElement("div");
+  private id: string;
+  constructor(wrapper: HTMLElement, config: UIConfig) {
+    this.id = (config.title + incrementer++).replace(/\s/g, "");
+
     this.wrapper.classList.add("group-wrapper");
+
+    const contentWrapper = document.createElement("div");
+    contentWrapper.classList.add("ui-group-content-wrapper");
 
     const toppart = document.createElement("div");
     toppart.classList.add("group-toppart");
-    toppart.addEventListener(
-      "click",
-      () => {
-        this.open = !this.open;
-        if (this.open) {
-          toppart.classList.add("group-open");
-          this.wrapper.classList.add("group-open");
-        } else {
-          toppart.classList.remove("group-open");
-          this.wrapper.classList.remove("group-open");
-        }
-      },
-      false
-    );
 
     const triangle = document.createElement("span");
     triangle.innerHTML = "◢";
     toppart.append(triangle);
 
     const _title = document.createElement("h3");
-    _title.innerHTML = title;
+    _title.innerHTML = config.title;
     toppart.append(_title);
 
     wrapper.append(toppart);
 
-    wrapper.append(this.wrapper);
+    contentWrapper.append(this.wrapper);
 
-    toppart.click();
+    wrapper.append(contentWrapper);
+
+    const updateGroupState = () => {
+      groupState[this.id] = this.open;
+      localStorage.setItem("groupState", JSON.stringify(groupState));
+    };
+
+    const open = () => {
+      toppart.classList.add("group-open");
+      this.wrapper.classList.add("group-open");
+      contentWrapper.style.maxHeight = this.wrapper.getBoundingClientRect().height + "px";
+
+      setTimeout(() => {
+        contentWrapper.style.maxHeight = "5000px";
+      }, 300);
+    };
+
+    const close = () => {
+      toppart.classList.remove("group-open");
+      this.wrapper.classList.remove("group-open");
+      contentWrapper.style.maxHeight = "0px";
+    };
+
+    toppart.addEventListener(
+      "click",
+      () => {
+        this.open = !this.open;
+        this.open ? open() : close();
+        updateGroupState();
+      },
+      false
+    );
+
+    setTimeout(() => {
+      if (this.id in groupState) {
+        this.open = groupState[this.id];
+        groupState[this.id] && open();
+      } else {
+        this.open = !!config.open;
+        config.open && open();
+      }
+    }, 0);
   }
 }
