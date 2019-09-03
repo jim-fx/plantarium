7ed7869535d69685b86008af5faf52a022175a6f
Max Richter <jim-x@web.de>
2019-09-03 19:53:49 +0200

    Apply variation in stem diameter to branches

--- src/components/model-generator/createBranchGeometry.ts
+++ src/components/model-generator/createBranchGeometry.ts
@@ -1,23 +1,24 @@
 import curveToArray from "./helper/curveToArray";
-import { tube, join } from "./geometry";
+import { join } from "./geometry";
+import tube from "./geometry/_tube";
 import noise from "./helper/noise";
 
 let geometry: TransferGeometry;
 let oldDescription: string;
 
-function getBranchDiameter(diameter: parameter, stemDiameter: number, i: number) {
+function getBranchDiameter(diameter: parameter, stemDiameter: parameter, i: number) {
   let v = 0.1;
 
-  if ("variation" in diameter) {
-    v = diameter.value - diameter.value * diameter.variation * ((noise.n1d(93815 + i * 200) + 1) / 2);
+  if ("variation" in stemDiameter) {
+    v = diameter.value - diameter.value * stemDiameter.variation * ((noise.n1d(93815 + i * 200) + 1) / 2);
   } else {
     v = diameter.value;
   }
 
   if (diameter.curve && diameter.curve.length) {
-    return curveToArray(diameter.curve).map((_v: number) => v * _v * stemDiameter);
+    return curveToArray(diameter.curve).map((_v: number) => v * _v * stemDiameter.value);
   } else {
-    return [0, 1 * v * stemDiameter];
+    return [0.0001, v * stemDiameter.value];
   }
 }
 
@@ -28,13 +29,15 @@ export default function(pd: plantDescription, settings: settings, skeletons: Flo
     return geometry;
   }*/
 
-  const diameter = getBranchDiameter(pd.branches.diameter, pd.stem.diameter.value, i);
+  const diameter = getBranchDiameter(pd.branches.diameter, pd.stem.diameter, i);
   const amount = skeletons.length;
   const resX = settings.stemResX || 3;
 
+  const lowestBranch = pd.branches.lowestBranch.value || 0;
+
   return join(
     ...skeletons.map((skeleton, i) => {
-      return tube(skeleton, diameter.map(v => v * (1 - i / amount)), resX);
+      return tube(skeleton, diameter.map(v => v * ((1 - i / amount) * (1 - lowestBranch))), resX);
     })
   );
 }
