faaf4ee9481cc3a775d1fd36f5a320b639010824
Max Richter <jim-x@web.de>
2019-09-19 15:43:16 +0200

    Add new icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,9 @@
 import arrow from "./arrow.svg";
 import branch from "./branch.svg";
+import checkmark from "./checkmark.svg";
 import cog from "./cog.svg";
 import cross from "./cross.svg";
+import io from "./io.svg";
 import leaf from "./leaf.svg";
 import stem from "./stem.svg";
 import triangle from "./triangle.svg";
@@ -9,8 +11,10 @@ import triangle from "./triangle.svg";
 interface icon {
   arrow: SVGElement;
   brach: SVGElement;
+  checkmark: SVGElement;
   cog: SVGElement;
   cross: SVGElement;
+  io: SVGElement;
   leaf: SVGElement;
   stem: SVGElement;
   triangle: SVGElement;
@@ -21,8 +25,10 @@ const exp: icon = <icon>{};
 const strings = {
   arrow: arrow,
   branch: branch,
+  checkmark: checkmark,
   cog: cog,
   cross: cross,
+  io: io,
   leaf: leaf,
   stem: stem,
   triangle: triangle
