51c12579298dc57f91e71c392e2f2e62032344fc
Max Richter <jim-x@web.de>
2019-09-21 17:58:40 +0200

    [HEROKU] Forgot to bind server to process.env.PORT

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const PROD = false;
+const PORT = process.env.PORT || 4000;
 
 PROD && app.use(require("helmet")());
 PROD && app.enable("trust proxy");
@@ -11,4 +12,4 @@ app.use("/graphql", require("./graphql")());
 
 app.get("/", (req, res) => res.send("https://jim-fx.github.io/plantarium"));
 
-app.listen(4000, () => console.log("Express GraphQL Server Now Running On SERVER_URL/graphql"));
+app.listen(PORT, () => console.log("Express GraphQL Server Now Running"));
