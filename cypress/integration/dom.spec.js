da567d0266066692d9a39d931e75db86f62a6ee9
Max Richter <jim-x@web.de>
2020-03-08 01:54:21 +0100

    reset git to working state

--- cypress/integration/dom.spec.js
+++
@@ -1,9 +0,0 @@
-describe("My First Test", function() {
-  it('finds the content "type"', function() {
-    cy.visit("http://localhost:8080");
-
-    cy.wait(1000);
-
-    cy.contains("branch").click();
-  });
-});
--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,22 @@
 //BROWSER
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import scss from "rollup-plugin-scss";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import liveServer from "rollup-plugin-live-server";
 import glslify from "rollup-plugin-glslify";
 import { terser } from "rollup-plugin-terser";
 import analyze from "rollup-plugin-analyzer";
 import svg from "./src/assets/rollup-plugin-svg";
 import visualizer from "rollup-plugin-visualizer";
-import typescript from "rollup-plugin-typescript";
-import graphql from "rollup-plugin-graphql";
-import replace from "rollup-plugin-replace";
+import replace from "@rollup/plugin-replace";
 import dotenv from "dotenv";
 dotenv.config();
 
 const PROD = process.env.ROLLUP_WATCH !== "true";
 
 const extensions = [".mjs", ".ts", ".js"];
-
-const server = {
-  input: "./server/index.ts",
-  plugins: [resolve({ extensions, preferBuiltins: true }), commonjs(), typescript(), json(), graphql()],
-  external: [
-    "url",
-    "events",
-    "path",
-    "zlib",
-    "http",
-    "net",
-    "fs",
-    "buffer",
-    "crypto",
-    "util",
-    "stream",
-    "tty",
-    "mongodb-client-encryption",
-    "assert",
-    "saslprep",
-    "cluster",
-    "dns",
-    "constants",
-    "string_decoder",
-    "tls",
-    "module"
-  ],
-  output: {
-    file: "build/server/server.js",
-    format: "cjs"
-  }
-};
 
 const client = {
   input: "./src/index.ts",
@@ -85,31 +51,31 @@ const client = {
     PROD && terser(),
 
     PROD &&
-      analyze({
-        summaryOnly: true,
-        limit: 10
-      }),
+    analyze({
+      summaryOnly: true,
+      limit: 10
+    }),
 
     PROD &&
-      visualizer({
-        sourcemap: true
-      }),
+    visualizer({
+      sourcemap: true
+    }),
 
     //Import GLSL Shaders
     glslify({ include: ["**/*.vert", "**/*.frag"] }),
 
     !PROD &&
-      liveServer({
-        root: "./build/client",
-        open: true,
-        quiet: true,
-        logLevel: 0
-      })
+    liveServer({
+      root: "./build",
+      open: true,
+      quiet: true,
+      logLevel: 0
+    })
   ],
 
   output: [
     {
-      file: "./build/client/bundle.js",
+      file: "./build/bundle.js",
       format: "iife",
       name: "plantarium_client",
       sourcemap: true
@@ -140,15 +106,15 @@ const clientModel = {
     PROD && terser(),
 
     PROD &&
-      analyze({
-        summaryOnly: true,
-        limit: 10
-      })
+    analyze({
+      summaryOnly: true,
+      limit: 10
+    })
   ],
 
   output: [
     {
-      file: "./build/client/generator.js",
+      file: "./build/generator.js",
       format: "iife",
       name: "plantarium_generator",
       sourcemap: true
@@ -181,15 +147,15 @@ const clientData = {
     PROD && terser(),
 
     PROD &&
-      analyze({
-        summaryOnly: true,
-        limit: 10
-      })
+    analyze({
+      summaryOnly: true,
+      limit: 10
+    })
   ],
 
   output: [
     {
-      file: "./build/client/dataService.js",
+      file: "./build/dataService.js",
       format: "iife",
       name: "plantarium_dataservice",
       sourcemap: true
@@ -216,17 +182,17 @@ const clientServiceWorker = {
     json(),
 
     PROD &&
-      analyze({
-        summaryOnly: true,
-        limit: 10
-      }),
+    analyze({
+      summaryOnly: true,
+      limit: 10
+    }),
 
     PROD && terser()
   ],
 
   output: [
     {
-      file: "./build/client/sw.js",
+      file: "./build/sw.js",
       format: "iife",
       name: "plantarium_serviceworker",
       sourcemap: true
@@ -234,4 +200,4 @@ const clientServiceWorker = {
   ]
 };
 
-export default [server, client, clientModel, clientData, clientServiceWorker];
+export default [client, clientModel, clientData, clientServiceWorker];
