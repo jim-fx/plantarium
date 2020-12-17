7943a6f6ddd1a3585928046a25298cb11760f301
Jim Richter <jim-x@web.de>
2020-12-18 00:31:17 +0100

    style: move all *.ending types to one fiel

---
+++ packages/@types/files.d.ts
@@ -0,0 +1,12 @@
+declare module '*.frag' {
+  const content: string;
+  export default content;
+}
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+declare module '*.vert' {
+  const content: string;
+  export default content;
+}
--- packages/@types/frag.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.frag' {
-  const content: string;
-  export default content;
-}
