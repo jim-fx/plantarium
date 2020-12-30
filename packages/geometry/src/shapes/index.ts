65190a5468fc93015830edf865a667a613e0ad94
Jim Richter <jim-x@web.de>
2020-12-30 19:19:17 +0100

    fix: some stuff

--- packages/geometry/src/shapes/index.ts
+++ packages/geometry/src/shapes/index.ts
@@ -1,4 +1,4 @@
 export { default as circle } from './circle';
 export { default as triangle } from './triangle';
 export { default as ring } from './ring';
-export { default as tube } from './tube';
+export { default as tube } from './tube_new';
