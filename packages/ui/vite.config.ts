c9d4aee3c970f23a55109ea660bb8ab43a875e61
Jim Richter <jim-x@web.de>
2021-08-03 03:29:34 +0200

    fix(ui): make paths relative

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     tsconfigPaths(),
     svelte({ compilerOptions: { customElement: true } }),
   ],
+  base: '',
   build: {
     minify: 'esbuild',
     brotliSize: false,
