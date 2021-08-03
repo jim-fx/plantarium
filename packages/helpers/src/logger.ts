5426f95bae931e2d7c747e06e23b5aa114ed6ba8
Jim Richter <jim-x@web.de>
2021-08-03 16:26:37 +0200

    feat(logger): colors, grouping, spacing

--- packages/helpers/src/logger.ts
+++ packages/helpers/src/logger.ts
@@ -2,24 +2,109 @@ interface Logger {
   (...args: unknown[]): void;
   warn(...args: unknown[]): void;
   error(err: Error): void;
+  group(...args: unknown[]): void;
 }
 
 let filters: string[] = [];
 let level = 2;
 
+let longestName = 0;
+
+const scopes = {};
+let currentIndex = 0;
+
+const colors = [
+  '#00ffff',
+  '#f0ffff',
+  '#f5f5dc',
+  '#000000',
+  '#0000ff',
+  '#a52a2a',
+  '#00ffff',
+  '#00008b',
+  '#008b8b',
+  '#a9a9a9',
+  '#006400',
+  '#bdb76b',
+  '#8b008b',
+  '#556b2f',
+  '#ff8c00',
+  '#9932cc',
+  '#8b0000',
+  '#e9967a',
+  '#9400d3',
+  '#ff00ff',
+  '#ffd700',
+  '#008000',
+  '#4b0082',
+  '#f0e68c',
+  '#add8e6',
+  '#e0ffff',
+  '#90ee90',
+  '#d3d3d3',
+  '#ffb6c1',
+  '#ffffe0',
+  '#00ff00',
+  '#ff00ff',
+  '#800000',
+  '#000080',
+  '#808000',
+  '#ffa500',
+  '#ffc0cb',
+  '#800080',
+  '#800080',
+  '#ff0000',
+  '#c0c0c0',
+  '#ffffff',
+  '#ffff00',
+];
+
 function log(scope: string): Logger {
-  const log = (...args: []) => {
+  longestName = Math.max(longestName, scope.length);
+
+  let myIndex = currentIndex;
+
+  scopes[scope] = colors[currentIndex];
+  currentIndex++;
+
+  const log = (...args: any[]) => {
     if ((!filters.length || filters.includes(scope)) && level === 0) {
-      console.log(`[${scope}]`, ...args);
+      // Make some logs better to read
+      if (typeof args[0] === 'string' && typeof args[1] === 'object') {
+        console.groupCollapsed(
+          `%c[${scope.padEnd(longestName, ' ')}]`,
+          `color: hsl(${myIndex * 30}deg 68% 64%); font-weight: bold;`,
+          args[0],
+        );
+        console.log(...args.slice(1));
+        console.groupEnd();
+        return;
+      }
+
+      console.log(
+        `%c[${scope.padEnd(longestName, ' ')}]`,
+        `color: hsl(${myIndex * 30}deg 68% 64%); font-weight: bold;`,
+        ...args,
+      );
     }
   };
 
+  log.group = (...args: []) => {
+    if (level < 1)
+      console.groupCollapsed(
+        `%c[${scope.padEnd(longestName, ' ')}]`,
+        `color: hsl(${myIndex * 30}deg 68% 64%); font-weight: bold;`,
+        ...args,
+      );
+  };
+
   log.warn = (...args: []) => {
-    if (level <= 1) console.warn(`[${scope}]`, ...args);
+    if (level <= 1)
+      console.warn(`[${scope.padEnd(longestName, ' ')}]`, ...args);
   };
 
   log.error = (err: Error) => {
-    console.error(`[${scope}]`, err);
+    console.error(`[${scope.padEnd(longestName, ' ')}]`, err);
   };
 
   return log;
