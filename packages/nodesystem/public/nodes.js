86cb67f194086cee4b4f5612546b7c9755c09a22
Jim Richter <jim-x@web.de>
2021-08-09 17:31:53 +0200

    feat(nodesystem): add slider node to example

--- packages/nodesystem/public/nodes.js
+++ packages/nodesystem/public/nodes.js
@@ -78,4 +78,28 @@ const picker = {
   },
 };
 
-export default [boolops, compare, picker];
+const slider = {
+  title: 'Slider',
+  type: 'slider',
+  outputs: ['number'],
+  parameters: {
+    value: {
+      type: 'number',
+      label: false,
+      inputType: 'slider',
+      internal: true,
+      min: 0,
+      max: 1,
+      step: 0.01,
+      value: 0,
+    },
+  },
+  computeNode(parameters) {
+    return {
+      type: 'slider',
+      parameters,
+    };
+  },
+};
+
+export default [boolops, compare, picker, slider];
