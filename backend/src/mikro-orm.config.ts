34c501a7af732632a1964899f08fd7071682bca7
Max Richter <jim-x@web.de>
2021-09-03 12:34:55 +0200

    feat(backend): allow mongodb database

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -2,22 +2,27 @@ import { Options } from '@mikro-orm/core';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 
-const type = 'sqlite';
-const host = 'localhost';
+let type = 'sqlite';
 let dbName = 'db.sql';
+let clientUrl;
 
 if (process.env.DB_NAME) {
-  dbName = process.env.DB_NAME;
+	dbName = process.env.DB_NAME;
+}
+
+if (process.env.MONGO_URL) {
+	type = 'mongo';
+	clientUrl = process.env.MONGO_URL;
 }
 
 const config: Options = {
-  type: type as 'sqlite' | 'mongo',
-  host,
-  dbName,
-  entities: ['dist/**/*.entity.js'],
-  entitiesTs: ['src/**/*.entity.ts'],
-  highlighter: new SqlHighlighter(),
-  metadataProvider: TsMorphMetadataProvider,
+	type: type as 'sqlite' | 'mongo',
+	dbName,
+	clientUrl,
+	entities: ['dist/**/*.entity.js'],
+	entitiesTs: ['src/**/*.entity.ts'],
+	highlighter: new SqlHighlighter(),
+	metadataProvider: TsMorphMetadataProvider,
 };
 
 export default config;
