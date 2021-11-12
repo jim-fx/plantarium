c1ec939c398c6e348e4169dde29402065df7a54e
Jim Richter <jim-x@web.de>
2021-11-12 17:42:15 +0100

    fix(pages): correct admin base path

--- frontend/svelte.config.cjs
+++ frontend/svelte.config.cjs
@@ -1,8 +1,6 @@
 const preprocess = require('svelte-preprocess');
 const { typescript } = require('svelte-preprocess-esbuild');
 
-const {ADMIN_PATH = "/"} = process.env;
-
 module.exports = {
   preprocess: [
     typescript(),
@@ -10,9 +8,4 @@ module.exports = {
       typescript: false,
     }),
   ],
-  kit: {
-    paths: {
-			base: ADMIN_PATH,
-		},
-  }
 };
