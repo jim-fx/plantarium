8e50c3fd13faccff38394c7f4d744a071da3086d
Jim Richter <jim-x@web.de>
2022-01-20 21:03:15 +0100

    fix: ci change frontend build folder

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -12,7 +12,6 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter(),
 		target: '#svelte',
