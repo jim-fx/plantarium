15fdc1ee99a076a1a1e74f3460858a20800ffd75
Max <jim-x@web.de>
2022-05-21 16:15:34 +0200

    fix(ui): InputCurve closes #73

--- packages/ui/src/lib/InputCurve.svelte
+++ packages/ui/src/lib/InputCurve.svelte
@@ -27,7 +27,11 @@
   const updateValue = () => {
     requestAnimationFrame(() => {
       dispatch('change', points);
-      points = points.sort((a, b) => (a.x > b.x ? -1 : 1));
+      points = points.sort((a, b) => (a.x > b.x ? 1 : -1));
+      points[0].y = 1;
+      points[0].x = 0;
+      points[points.length - 1].x = 1;
+      points[points.length - 1].y = 0;
     });
   };
 
