0f0d4db2301d9220f54dd80929559c5c92bf4f20
Max <jim-x@web.de>
2022-05-25 01:00:43 +0200

    fix: remove test wasm folder

--- packages/generator-wasm/src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-
-declare module "*.zig" {
-  const w = WebAssembly;
-  export const instantiate = (importObject?: { [key: string]: any }) => Promise<{ exports: { [key: string]: any } }>();
-}
--- packages/generator-wasm/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import zig from "vite-plugin-zig"
-
-export default {
-  plugins: [zig()],
-  build: { target: "esnext" }
-}
