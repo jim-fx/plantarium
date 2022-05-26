32142c94c87572aabba8522528ce6cad7c6df274
Max <jim-x@web.de>
2022-05-26 23:41:25 +0200

    feat: trying to pnpm publish

--- packages/nodesystem/src/lib/index.ts
+++ packages/nodesystem/src/lib/index.ts
@@ -4,5 +4,4 @@ export { default as NodeView } from './view/NodeView';
 export { default as NodeState } from "./model/NodeState"
 export { default as NodeSystem } from './model/NodeSystem';
 export { checkNodeType } from "./types"
-
 export type { NodeProps } from "./types"
