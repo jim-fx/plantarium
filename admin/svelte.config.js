50cce7cb3c735b155651661abc0f24165e8630f1
Max Richter <jim-x@web.de>
2023-04-13 21:52:45 +0200

    fix: trying to fix gh pages

--- admin/svelte.config.js
+++ admin/svelte.config.js
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+let { BASE_PATH = '', IS_GH_PAGES = false } = process.env;
+if (IS_GH_PAGES) {
+  BASE_PATH = '/admin';
+}
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,9 +12,11 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter({
-      fallback: "200.html"
-    }),
+    adapter: adapter(),
+
+    paths: {
+      base: BASE_PATH,
+    },
   },
 };
 
