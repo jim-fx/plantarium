a2e9558fa3f6a07f994a90b10f08671a791cce7e
Max Richter <jim-x@web.de>
2023-11-16 14:26:25 +0100

    fix(admin): missing import:

--- apps/backend/src/projects/dto/update-project.dto.ts
+++ apps/backend/src/projects/dto/update-project.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProjectDto } from './create-project.dto';
+import { ProjectType } from '../entities/project.entity';
 
 export class UpdateProjectDto extends PartialType(CreateProjectDto) {
   type: ProjectType;
